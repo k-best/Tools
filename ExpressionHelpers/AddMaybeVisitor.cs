b1fefd1fa3f6d569b0fabdcc9cfef2894b6af036
Kirill Bestemyanov <k_best@inbox.ru>
2015-04-21 00:07:11 +0300

    AddMaybeMonad ExpressionVisitor

---
+++ ExpressionHelpers/AddMaybeVisitor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionHelpers
+{
+    public class AddMaybeVisitor : ExpressionVisitor
+    {
+        public Expression<Func<T1, T2>> Modify<T1, T2>(Expression<Func<T1, T2>> expression)
+        {
+            return (Expression<Func<T1, T2>>)Visit(expression);
+        }
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            Visit(node.Expression);
+
+            var expressionType = node.Expression.Type;
+            var memberType = node.Type;
+
+            var withMethodinfo = typeof(AddMaybeVisitor)
+                .GetMethod("With")
+                .MakeGenericMethod(expressionType, memberType);
+
+            var p = Expression.Parameter(expressionType);
+            var l = Expression.Lambda(Expression.MakeMemberAccess(p, node.Member), p);
+
+            return Expression.Call(withMethodinfo,
+                node.Expression,
+                Expression.Constant(l.Compile(), typeof(Func<,>).MakeGenericType(expressionType, memberType))
+                );
+        }
+
+        public static TResult With<TSource, TResult>(TSource source, Func<TSource, TResult> action) where TSource : class
+        {
+            if (source != default(TSource))
+                return action(source);
+            return default(TResult);
+        }
+    }
+}
