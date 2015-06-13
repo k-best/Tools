9910c3c6f8a546d928870fccf716bcd8033ea114
k-best <k_best@inbox.ru>
2015-06-13 21:23:31 +0300

    добавил тестов на валидатор и поправил его чтобы тесты проходили

--- Wcf.AttributeValidation/Wcf.AttributeValidation.Test/ObjectValidatorTest.cs
+++ Wcf.AttributeValidation/Wcf.AttributeValidation.Test/ObjectValidatorTest.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Wcf.AttributeValidation.Test
@@ -7,8 +11,109 @@ namespace Wcf.AttributeValidation.Test
     public class ObjectValidatorTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldValidateObjectWithIEnumerable()
         {
+            var objectToValidate = new ObjectWithIEnumerable()
+            {
+                Collection = new InnerObject []{new InnerObject(), new InnerObject{Value1 = "element"}, new InnerObject(), }
+            };
+            var validator = new ObjectValidator();
+
+            var result = validator.Validate(new[] {objectToValidate}, null).ToArray();
+            Assert.IsTrue(result.Any());
+            Assert.AreEqual(2, result.Length);
         }
+        [TestMethod]
+        public void ShouldValidateIEnumerable()
+        {
+            var objectToValidate = GetEnumerator();
+            var validator = new ObjectValidator();
+
+            var result = validator.Validate(new[] { objectToValidate }, null).ToArray();
+            Assert.IsTrue(result.Any());
+            Assert.AreEqual(2, result.Length);
+        }
+
+        [TestMethod]
+        public void ShouldValidateLargeArrays()
+        {
+            const int length = 1000000;
+            var objectToValidate = new InnerObject[length];
+            for (int i = 0; i < length; i++)
+            {
+                objectToValidate[i]=new InnerObject();
+            }
+            var validator = new ObjectValidator();
+
+            var result = validator.Validate(new[] { objectToValidate }, null).ToArray();
+            Assert.IsTrue(result.Any());
+            Assert.AreEqual(length, result.Length);
+        }
+
+        private IEnumerable<InnerObject> GetEnumerator()
+        {
+            yield return new InnerObject{Value1="11"};
+            yield return new InnerObject();
+            yield return new InnerObject();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ShouldNotThrowStackOverflowExceptionOnCircullarReference()
+        {
+            var objectToValidate = new ObjectWithCircularReference {Name = "object1"};
+            objectToValidate.RelatedObject = objectToValidate;
+
+            var validator = new ObjectValidator();
+
+            var result = validator.Validate(new[] { objectToValidate }, null).ToArray();
+        }
+    }
+
+    internal class ObjectWithCircularReference
+    {
+        public ObjectWithCircularReference RelatedObject { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    internal class ObjectWithIEnumerable
+    {
+        [NotEmptyEnumerable]
+        public IEnumerable<InnerObject> Collection { get; set; } 
+    }
+
+    internal class NotEmptyEnumerableAttribute :ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var collection = value as IEnumerable;
+            return collection == null || collection.Cast<object>().Any();
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (IsValid(value)) return ValidationResult.Success;
+            string[] memberNames;
+
+            if (validationContext == null || validationContext.MemberName == null)
+                memberNames = new string[0];
+            else
+            {
+                memberNames = new string[1]
+                {
+                    validationContext.MemberName
+                };
+            }
+            var result = new ValidationResult("Collection is empty",
+                memberNames);
+            return result;
+        }
+    }
+
+    internal class InnerObject
+    {
+        [Required]
+        public string Value1 { get; set; }
     }
 }
--- Wcf.AttributeValidation/Wcf.AttributeValidation/ObjectValidator.cs
+++ Wcf.AttributeValidation/Wcf.AttributeValidation/ObjectValidator.cs
@@ -10,26 +10,27 @@ namespace Wcf.AttributeValidation
 {
     public class ObjectValidator : IValidator
     {
-        private int _safetyCounter;
-
         public IEnumerable<ValidationResult> Validate(IEnumerable<object> inputs, IServiceProvider serviceProvider)
         {
             return inputs.Aggregate((IEnumerable<ValidationResult>)new ValidationResult[0],
                 (current, input) =>
-                    current.Concat(ValidateObject(input, string.Empty, serviceProvider)));
+                    current.Concat(ValidateObject(input, string.Empty, 0, serviceProvider)));
         }
 
-        private IEnumerable<ValidationResult> ValidateObject(object instance, string prefix, IServiceProvider serviceProvider)
+        private IEnumerable<ValidationResult> ValidateObject(object instance, string prefix, int recursionDepth, IServiceProvider serviceProvider)
         {
             if (instance == null)
                 return new ValidationResult[0];
+            if(recursionDepth>1000)
+                throw new InvalidOperationException("Possible circullar reference");
             var validationContext = new ValidationContext(instance, serviceProvider, null);
+            var newRecursionDepth = recursionDepth + 1;
             return
                 ValidateAttributes(instance, validationContext, prefix)
                     .Concat(ValidateIValidatableObject(instance, validationContext, prefix))
                     .Concat(
                         GetChildComplexObjects(instance)
-                            .SelectMany(c => ValidateObject(c.Item1, prefix + c.Item2 + ".", serviceProvider)))
+                            .SelectMany(c => ValidateObject(c.Item1, prefix + c.Item2 + ".", newRecursionDepth, serviceProvider)))
                     .ToArray();
         }
 
@@ -86,13 +87,13 @@ namespace Wcf.AttributeValidation
         private IEnumerable<Tuple<object, string>> GetChildComplexObjects(object instance)
         {
             Debug.Assert(instance != null);
-            if (_safetyCounter > 100000)
-                throw new InvalidOperationException("Possible circullar references");
-            _safetyCounter++;
-            //если массив, то у него нет комплексных дочерних свойств
+            //если коллекция, то у нее нет комплексных дочерних свойств
             var enumerableInstance = instance as IEnumerable;
             if (enumerableInstance != null)
+            {
                 foreach (var tuple in ValidateCollection(enumerableInstance, string.Empty)) yield return tuple;
+                yield break;
+            }
             var propDescriptors =
                 TypeDescriptor.GetProperties(instance).Cast<PropertyDescriptor>().ToArray();
             foreach (var propertyDescriptor in propDescriptors)
@@ -100,11 +101,6 @@ namespace Wcf.AttributeValidation
                 var value = propertyDescriptor.GetValue(instance);
                 if (value == null)
                     continue;
-                var enumerableValue = value as IEnumerable;
-                if (enumerableValue!=null)
-                {
-                    foreach (var tuple in ValidateCollection(enumerableValue, propertyDescriptor.Name)) yield return tuple;
-                }
                 if (IsComplexType(propertyDescriptor))
                     yield return Tuple.Create(value, propertyDescriptor.Name);
             }
