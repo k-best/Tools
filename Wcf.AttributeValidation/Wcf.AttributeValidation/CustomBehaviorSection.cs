cbbe523c8ce8cd8c73d8b669f06fc783c54f8b0a
Kirill Bestemyanov <k_best@inbox.ru>
2015-06-30 00:14:02 +0300

    Added nuspec and transforms for nuget package

--- Wcf.AttributeValidation/Wcf.AttributeValidation/CustomBehaviorSection.cs
+++ Wcf.AttributeValidation/Wcf.AttributeValidation/CustomBehaviorSection.cs
@@ -11,29 +11,12 @@ namespace Wcf.AttributeValidation
     public class CustomBehaviorSection : BehaviorExtensionElement
     {
         private const string EnabledAttributeName = "enabled";
-        private const string ValidatorTypeName = "type";
-
+       
         [ConfigurationProperty(EnabledAttributeName, DefaultValue = true, IsRequired = false)]
         public bool Enabled
         {
             get { return (bool)base[EnabledAttributeName]; }
             set { base[EnabledAttributeName] = value; }
-        }
-
-        [TypeConverter(typeof(TypeNameConverter))]
-        [ConfigurationProperty(ValidatorTypeName, IsRequired = true)]
-        public Type ValidatorType
-        {
-            get
-            {
-                var validatorType = base[ValidatorTypeName] as Type;
-                if(validatorType==null)
-                    throw new ConfigurationErrorsException("Type of ValidatorType is not found or it does not implement IValidator interface");
-                var interfaces = validatorType.GetInterfaces();
-                if (interfaces == null || !interfaces.Contains(typeof(IValidator)))
-                    throw new ConfigurationErrorsException("Type of ValidatorType is not found or it does not implement IValidator interface");
-                return validatorType;
-            }
         }
 
         protected override object CreateBehavior()
